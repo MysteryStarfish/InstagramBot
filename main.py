93cd0222897aef0cbebe99f022b1ceefc655d16b
mysterystarfish <paulhsu1016@gmail.com>
2024-08-07 01:09:50 +0800

    update

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
 from time import sleep
 import random
-
 from instagrapi import Client
-
 import dotenv
 
 dotenv.load_dotenv(".env")
@@ -16,6 +14,7 @@ auto_reply_ids = ['65206241525']
 
 cl = Client()
 cl.login(USERNAME, PASSWORD)
+
 thread = cl.direct_threads(1)[0]
 pre_message = thread.messages[0]
 
@@ -28,32 +27,33 @@ def get_random_message():
     messages = ["確實", "真的欸", "有料", "要", "超好笑", "笑死", "嗯嗯", "喔不", "哇嘞"]
     message = random.choice(messages)
     return message
-
-is_important_message = False
-is_no_new_message = False
-
+    
 while True:
+    sleep(1)
     current_message = get_latest_message(cl)
-    if current_message.user_id == author_id:
+    if current_message == pre_message:
         continue
     elif current_message.user_id not in auto_reply_ids:
-        is_important_message = True
-    elif current_message == pre_message:
-        is_no_new_message = True
-    
-    if is_no_new_message:
-        print("No new message")
-        continue
-    if is_important_message:
-        print("Important Message!!!")
         continue
 
-    is_important_message = False
-    is_no_new_message = False
+    if current_message.user_id == author_id:
+        if current_message.text.startswith("add"):
+            user_name = current_message.text.split()[1]
+            user_id = cl.user_id_from_username(user_name)
+            auto_reply_ids.append(user_id)
+            cl.direct_send(f"已新增 {user_name} 至敷衍區", user_ids = [author_id])
+        elif current_message.text.startswith("remove"):
+            user_name = current_message.text.split()[1]
+            user_id = cl.user_id_from_username(user_name)
+            auto_reply_ids.remove(user_id)
+            cl.direct_send(f"已移除 {user_name} 從敷衍區", user_ids = [author_id])
+        continue
 
     pre_message = current_message
-    print("New message")
+    print("New message: ", current_message.text)
 
-    cl.direct_send(get_random_message(), user_ids = [current_message.user_id])
-    
-    sleep(1)
+    reply_message = get_random_message()
+    cl.direct_send(reply_message, user_ids = [current_message.user_id])
+    print("Reply with: ", reply_message)
+
+    print()
